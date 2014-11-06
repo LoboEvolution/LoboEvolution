615507b59318adf465889d1763969cdbe9c1fbda
Ivan Di Francesco <ivan.difrancesco@yahoo.it>
2014-11-06 18:12:52 +0100

    Fix Project

--- HTML_Renderer/org/lobobrowser/html/w3c/HTMLTableHeaderCellElement.java
+++
@@ -1,8 +0,0 @@
-package org.lobobrowser.html.w3c;
-
-public interface HTMLTableHeaderCellElement extends HTMLTableCellElement {
-	// HTMLTableHeaderCellElement
-	public String getScope();
-
-	public void setScope(String scope);
-}
--- HTML_Renderer/org/lobobrowser/html/w3c/PropertyNodeList.java
+++
@@ -1,8 +0,0 @@
-package org.lobobrowser.html.w3c;
-
-import org.w3c.dom.NodeList;
-
-public interface PropertyNodeList extends NodeList {
-	// PropertyNodeList
-	public Object[] getValues();
-}
