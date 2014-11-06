8e3018815f55a33e4a0cb41c8f68c69af0a2d987
Ivan Di Francesco <ivan.difrancesco@yahoo.it>
2014-11-06 18:56:51 +0100

    Fix project

---
+++ HTML_Renderer/src/org/lobobrowser/html/w3c/HTMLVideoElement.java
@@ -0,0 +1,20 @@
+package org.lobobrowser.html.w3c;
+
+public interface HTMLVideoElement extends HTMLMediaElement {
+	// HTMLVideoElement
+	public String getWidth();
+
+	public void setWidth(String width);
+
+	public String getHeight();
+
+	public void setHeight(String height);
+
+	public int getVideoWidth();
+
+	public int getVideoHeight();
+
+	public String getPoster();
+
+	public void setPoster(String poster);
+}
