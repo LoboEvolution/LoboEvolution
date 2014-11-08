75e19fbf003628486947565f352c81ebc0f4a1f1
Ivan Di Francesco <ivan.difrancesco@yahoo.it>
2014-11-08 01:00:53 +0100

    Delete broke package

--- HTML_Renderer/org/lobobrowser/html/w3c/HTMLDetailsElement.java
+++
@@ -1,8 +0,0 @@
-package org.lobobrowser.html.w3c;
-
-public interface HTMLDetailsElement extends HTMLElement {
-	// HTMLDetailsElement
-	public boolean getOpen();
-
-	public void setOpen(boolean open);
-}
