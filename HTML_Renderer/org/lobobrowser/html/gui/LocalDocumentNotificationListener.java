9e1f355b8e960f8fe91771934949c580905aa5fb
Ivan Di Francesco <ivan.difrancesco@yahoo.it>
2014-11-23 12:04:04 +0100

    Add missing class

---
+++ HTML_Renderer/org/lobobrowser/html/gui/LocalDocumentNotificationListener.java
@@ -0,0 +1,73 @@
+/*
+    GNU LESSER GENERAL PUBLIC LICENSE
+    Copyright (C) 2006 The XAMJ Project
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+    Contact info: lobochief@users.sourceforge.net
+ */
+package org.lobobrowser.html.gui;
+
+import org.lobobrowser.html.dombl.DocumentNotificationListener;
+import org.lobobrowser.html.dombl.NodeImpl;
+
+public class LocalDocumentNotificationListener implements
+		DocumentNotificationListener {
+	public void allInvalidated() {
+		HtmlPanel html = new HtmlPanel();
+		html.addNotification(new DocumentNotification(
+				DocumentNotification.GENERIC, null));
+	}
+
+	public void invalidated(NodeImpl node) {
+		HtmlPanel html = new HtmlPanel();
+		html.addNotification(new DocumentNotification(
+				DocumentNotification.GENERIC, node));
+	}
+
+	public void lookInvalidated(NodeImpl node) {
+		HtmlPanel html = new HtmlPanel();
+		html.addNotification(new DocumentNotification(
+				DocumentNotification.LOOK, node));
+	}
+
+	public void positionInvalidated(NodeImpl node) {
+		HtmlPanel html = new HtmlPanel();
+		html.addNotification(new DocumentNotification(
+				DocumentNotification.POSITION, node));
+	}
+
+	public void sizeInvalidated(NodeImpl node) {
+		HtmlPanel html = new HtmlPanel();
+		html.addNotification(new DocumentNotification(
+				DocumentNotification.SIZE, node));
+	}
+
+	public void externalScriptLoading(NodeImpl node) {
+		// Ignorable here.
+	}
+
+	public void nodeLoaded(NodeImpl node) {
+		HtmlPanel html = new HtmlPanel();
+		html.addNotification(new DocumentNotification(
+				DocumentNotification.GENERIC, node));
+	}
+
+	public void structureInvalidated(NodeImpl node) {
+		HtmlPanel html = new HtmlPanel();
+		html.addNotification(new DocumentNotification(
+				DocumentNotification.GENERIC, node));
+	}
+}
---
+++ HTML_Renderer/org/lobobrowser/html/js/LocalSecureClassLoader.java
@@ -0,0 +1,44 @@
+/*
+    GNU LESSER GENERAL PUBLIC LICENSE
+    Copyright (C) 2006 The XAMJ Project
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+    Contact info: lobochief@users.sourceforge.net
+ */
+package org.lobobrowser.html.js;
+
+import java.security.CodeSource;
+import java.security.SecureClassLoader;
+
+import org.mozilla.javascript.GeneratedClassLoader;
+
+public class LocalSecureClassLoader extends SecureClassLoader implements GeneratedClassLoader {
+	private CodeSource codesource;
+	
+	public LocalSecureClassLoader(ClassLoader parent,CodeSource codesource) {
+		super(parent);
+		this.codesource = codesource;
+	}
+
+	@Override
+	public Class defineClass(String name, byte[] b) {
+		return this.defineClass(name, b, 0, b.length, codesource);
+	}
+
+	public void linkClass(Class clazz) {
+		super.resolveClass(clazz);
+	}
+}
