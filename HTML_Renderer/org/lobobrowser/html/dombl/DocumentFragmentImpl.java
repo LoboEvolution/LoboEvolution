1684096d490ee893aa0978521fe9b611b5d749b7
Ivan Di Francesco <ivan.difrancesco@yahoo.it>
2015-01-08 17:28:53 +0100

    DocumentFragment add methods querySelector and querySelectorAll(the method is not finish)

--- HTML_Renderer/org/lobobrowser/html/dombl/DocumentFragmentImpl.java
+++ HTML_Renderer/org/lobobrowser/html/dombl/DocumentFragmentImpl.java
@@ -23,11 +23,16 @@
  */
 package org.lobobrowser.html.dombl;
 
+import org.lobobrowser.html.domfilter.ElementIdFilter;
+import org.lobobrowser.html.domimpl.HTMLDocumentImpl;
+import org.lobobrowser.html.w3c.HTMLDocumentFragment;
 import org.w3c.dom.DOMException;
 import org.w3c.dom.DocumentFragment;
+import org.w3c.dom.Element;
 import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
-public class DocumentFragmentImpl extends NodeImpl implements DocumentFragment {
+public class DocumentFragmentImpl extends NodeImpl implements DocumentFragment,HTMLDocumentFragment {
 	public DocumentFragmentImpl() {
 		super();
 	}
@@ -53,5 +58,52 @@ public class DocumentFragmentImpl extends NodeImpl implements DocumentFragment {
 
 	protected Node createSimilarNode() {
 		return new DocumentFragmentImpl();
+	}
+
+	@Override
+	public Element querySelector(String selectors) {
+		HTMLDocumentImpl doc = (HTMLDocumentImpl) this.document;
+		Element element = null;
+		if (selectors.startsWith("#"))
+			element = doc.getElementById(selectors.replace("#", ""));
+		if (selectors.startsWith("."))
+			element = (Element) doc.getElementsByClassName(selectors.replace(".", "")).item(0);
+		return element;
+	}
+
+	@Override
+	public NodeList querySelectorAll(String selectors) {
+		HTMLDocumentImpl doc = (HTMLDocumentImpl) this.document;
+		NodeList list = null;
+
+		if (selectors.contains(",")) {
+			String[] strList = selectors.split(",");
+
+			for (int i = 0; i < strList.length; i++) {
+
+				String node = strList[i];
+
+				if (list == null) {
+					list = getList(doc, node);
+				} else {// TODO}
+				}
+			}
+		} else {
+			list = getList(doc, selectors);
+		}
+		return list;
+	}
+	
+	private NodeList getList(HTMLDocumentImpl doc,String selectors){
+		NodeList list = null;
+		
+		if (selectors.startsWith("#")) {
+			list = getNodeList(new ElementIdFilter(selectors.replace("#", "")));
+		}
+		if (selectors.startsWith(".")) {
+			list = doc.getElementsByClassName(selectors.replace(".", ""));
+		}
+		
+		return list;
 	}
 }
