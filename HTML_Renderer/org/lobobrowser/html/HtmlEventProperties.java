e2f90e143917d7acaac74a6f9ee6f9ff914faa35
Ivan Di Francesco <ivan.difrancesco@yahoo.it>
2015-02-08 18:28:19 +0100

    Add event properties

---
+++ HTML_Renderer/org/lobobrowser/html/HtmlEventProperties.java
@@ -0,0 +1,19 @@
+package org.lobobrowser.html;
+
+public interface HtmlEventProperties {
+
+	public final String UIEVENT = "UIEvent";
+	public final String UIEVENTS = "UIEvents";
+	public final String MOUSEEVENT = "MouseEvent";
+	public final String MOUSEEVENTS = "MouseEvents";
+	public final String MUTATIONEVENT = "MutationEvent";
+	public final String MUTATIONEVENTS = "MutationEvents";
+	public final String MUTATIONNAMEEVENT = "MutationNameEvent";
+	public final String TEXTEVENT = "TextEvent";
+	public final String KEYBOARDEVENT = "KeyboardEvent";
+	public final String CUSTOMEVENT = "CustomEvent";
+	public final String EVENT = "Event";
+	public final String SVGEVENTS = "SVGEvents";
+	public final String SVGZOOMEVENTS = "SVGZoomEvents";
+
+}
