83ce3048459c7a863afe1b294b141a3c8ebdd278
Ivan Di Francesco <ivan.difrancesco@yahoo.it>
2015-06-19 14:59:54 +0200

    Add CSSFormat and CSSFormatable

---
+++ parser/src/main/java/com/steadystate/css/format/CSSFormatable.java
@@ -0,0 +1,44 @@
+/*
+ * CSS Parser Project
+ *
+ * Copyright (C) 1999-2015 David Schweinsberg.  All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * To contact the authors of the library:
+ *
+ * http://cssparser.sourceforge.net/
+ * mailto:davidsch@users.sourceforge.net
+ *
+ */
+
+package com.steadystate.css.format;
+
+/**
+ * Common interface for all classes supporting formated output.
+ *
+ * @author rbri
+ */
+public interface CSSFormatable {
+
+    /**
+     * Returns a string representation of the rule based on the given format.
+     * If provided format is null, the result is the same as getCssText()
+     *
+     * @param format the formatting rules
+     * @return the formated string
+     */
+    String getCssText(final CSSFormat format);
+}
