986568169433f7b659bba09d22270a35b4153ef4
Ivan Di Francesco <ivan.difrancesco@yahoo.it>
2015-07-23 12:58:32 +0200

    Improve PDFRender width fork https://github.com/katjas/PDFrenderer

---
+++ Primary_Extension/src/main/java/org/lobobrowser/primary/gui/pdf/PageChangeListener.java
@@ -0,0 +1,32 @@
+package org.lobobrowser.primary.gui.pdf;
+/*
+ * $Id: PageChangeListener.java,v 1.2 2007/12/20 18:33:33 rbair Exp $
+ *
+ * Copyright 2004 Sun Microsystems, Inc., 4150 Network Circle,
+ * Santa Clara, California 95054, U.S.A. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+/**
+ * A listener to indicate that some control has asked for a particular page to
+ * be displayed.
+ */
+public interface PageChangeListener {
+	/**
+	 * Notifies the listener that it should go to a particular page number.
+	 */
+	public void gotoPage(int page);
+}
