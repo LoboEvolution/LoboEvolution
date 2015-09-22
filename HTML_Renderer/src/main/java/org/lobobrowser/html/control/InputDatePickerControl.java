e7e377a67ff4285b12a10535b8568aa86eaeb4b8
Ivan Di Francesco <ivan.difrancesco@yahoo.it>
2015-09-22 12:26:54 +0200

    Implemented input type date attributes: value, min, max, pattern. Set background red when validation fails.

--- HTML_Renderer/src/main/java/org/lobobrowser/html/control/InputDatePickerControl.java
+++ HTML_Renderer/src/main/java/org/lobobrowser/html/control/InputDatePickerControl.java
@@ -14,30 +14,85 @@
  */
 package org.lobobrowser.html.control;
 
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.swing.BorderFactory;
 import javax.swing.JComponent;
 
-import org.jdatepicker.DatePicker;
 import org.jdatepicker.JDatePicker;
+import org.lobobrowser.html.HtmlAttributeProperties;
 import org.lobobrowser.html.domimpl.HTMLBaseInputElement;
 import org.lobobrowser.util.gui.WrapperLayout;
 
 public class InputDatePickerControl extends BaseInputControl {
 
 	private static final long serialVersionUID = 1L;
+	private static String datePattern = "dd/MM/yyyy";
+	private String min = "";
+	private String max = "";
 
 	public InputDatePickerControl(HTMLBaseInputElement modelNode) {
 		super(modelNode);
-		this.setLayout(WrapperLayout.getInstance());	
-		DatePicker picker = new JDatePicker();
+		this.setLayout(WrapperLayout.getInstance());
+		JDatePicker picker = new JDatePicker();
 		picker.setTextEditable(true);
 		picker.setShowYearButtons(true);
-		this.add((JComponent) picker);	
+		min = modelNode.getAttribute(HtmlAttributeProperties.MIN);
+		max = modelNode.getAttribute(HtmlAttributeProperties.MAX);
+
+		if(modelNode.getPattern()!= null && modelNode.getPattern().length()>0){
+			datePattern = modelNode.getPattern();
+		}
+		
+		picker.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Calendar selectedValue = (Calendar) picker.getModel().getValue();
+				Date selectedDate = selectedValue.getTime();
+				SimpleDateFormat dateFormatter = new SimpleDateFormat(datePattern);
+				modelNode.setValue(dateFormatter.format(selectedDate));
+				if (isMin(selectedDate) || isMax(selectedDate)) {
+					picker.setBorder(BorderFactory.createLineBorder(Color.RED));
+				} else {
+					picker.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+				}
+			}
+		});
+
+		this.add((JComponent) picker);
 	}
 
 	@Override
 	public void resetInput() {
 		// TODO Auto-generated method stub
 
+	}
+	
+	private boolean isMin(Date keyCode) {
+		try {
+			if (keyCode == null)
+				return true;
+			SimpleDateFormat dateFormatter = new SimpleDateFormat(datePattern);
+			return keyCode.before(dateFormatter.parse(min));
+		} catch (Exception ex) {
+			return false;
+		}
+	}
+
+	private boolean isMax(Date keyCode) {
+		try {
+			if (keyCode == null)
+				return true;
+			SimpleDateFormat dateFormatter = new SimpleDateFormat(datePattern);
+			return keyCode.after(dateFormatter.parse(max));
+		} catch (Exception ex) {
+			return false;
+		}
 	}
 
 }
