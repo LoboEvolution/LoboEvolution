eb155e3d934e639659d0f3d7763841c46929b51c
Ivan Di Francesco <ivan.difrancesco@yahoo.it>
2016-01-01 12:48:28 +0100

    Happy New Year

--- Common/src/main/java/org/lobobrowser/util/gui/FontKey.java
+++ Common/src/main/java/org/lobobrowser/util/gui/FontKey.java
@@ -1,6 +1,6 @@
 /*
  * GNU GENERAL LICENSE Copyright (C) 2006 The Lobo Project. Copyright (C) 2014 -
- * 2015 Lobo Evolution This program is free software; you can redistribute it
+ * 2016 Lobo Evolution This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General Public License as
  * published by the Free Software Foundation; either verion 2 of the License, or
  * (at your option) any later version. This program is distributed in the hope
--- HTML_Renderer/src/main/java/org/lobobrowser/html/style/selectors/SelectorMatcher.java
+++ HTML_Renderer/src/main/java/org/lobobrowser/html/style/selectors/SelectorMatcher.java
@@ -1,6 +1,6 @@
 /*
  * GNU LESSER GENERAL PUBLIC LICENSE Copyright (C) 2006 The Lobo Project.
- * Copyright (C) 2014 - 2015 Lobo Evolution This library is free software; you
+ * Copyright (C) 2014 - 2016 Lobo Evolution This library is free software; you
  * can redistribute it and/or modify it under the terms of the GNU Lesser
  * General Public License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version. This
