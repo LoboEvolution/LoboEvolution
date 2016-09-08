7c71a19ca6ca79973bdf3749348dd9068c7548cb
Ivan Di Francesco <ivan.difrancesco@yahoo.it>
2016-09-08 21:28:46 +0200

    Update from GPL2 to GPL3

--- HTML_Renderer/src/main/java/org/lobobrowser/html/domimpl/DOMFragmentImpl.java
+++ HTML_Renderer/src/main/java/org/lobobrowser/html/domimpl/DOMFragmentImpl.java
@@ -5,7 +5,7 @@
     This program is free software; you can redistribute it and/or
     modify it under the terms of the GNU General Public
     License as published by the Free Software Foundation; either
-    verion 2 of the License, or (at your option) any later version.
+    verion 3 of the License, or (at your option) any later version.
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -13,8 +13,8 @@
     General License for more details.
 
     You should have received a copy of the GNU General Public
-    License along with this library; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
 
     Contact info: lobochief@users.sourceforge.net; ivan.difrancesco@yahoo.it
  */
@@ -85,7 +85,7 @@ HTMLDocumentFragment {
      */
     @Override
     public short getNodeType() {
-        return org.w3c.dom.Node.DOCUMENT_FRAGMENT_NODE;
+        return Node.DOCUMENT_FRAGMENT_NODE;
     }
 
     /*
