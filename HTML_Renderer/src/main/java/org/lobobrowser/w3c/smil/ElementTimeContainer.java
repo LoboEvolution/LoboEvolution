845458ba62f66055378ea06d3c2a140b95d27638
Ivan Di Francesco <ivan.difrancesco@yahoo.it>
2017-06-27 23:46:13 +0200

    Added SMIL Animation interfaces #13

---
+++ HTML_Renderer/src/main/java/org/lobobrowser/w3c/smil/ElementTimeContainer.java
@@ -0,0 +1,48 @@
+/*
+    GNU GENERAL LICENSE
+    Copyright (C) 2006 The Lobo Project. Copyright (C) 2014 - 2017 Lobo Evolution
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public
+    License as published by the Free Software Foundation; either
+    verion 3 of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General License for more details.
+
+    You should have received a copy of the GNU General Public
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
+
+    Contact info: lobochief@users.sourceforge.net; ivan.difrancesco@yahoo.it
+ */
+
+package org.lobobrowser.w3c.smil;
+
+import org.w3c.dom.NodeList;
+
+/**
+ *  This is a placeholder - subject to change. This represents generic 
+ * timelines. 
+ */
+public interface ElementTimeContainer extends ElementTime {
+    /**
+     *  A NodeList that contains all timed childrens of this node. If there are
+     *  no timed children, the <code>Nodelist</code> is empty.  An iterator 
+     * is more appropriate here than a node list but it requires Traversal 
+     * module support. 
+     */
+    public NodeList getTimeChildren();
+
+    /**
+     *  Returns a list of child elements active at the specified invocation. 
+     * @param instant  The desired position on the local timeline in 
+     *   milliseconds. 
+     * @return  List of timed child-elements active at instant. 
+     */
+    public NodeList getActiveChildrenAt(float instant);
+
+}
+
---
+++ HTML_Renderer/src/main/java/org/lobobrowser/w3c/smil/SMILAnimateElement.java
@@ -0,0 +1,29 @@
+/*
+    GNU GENERAL LICENSE
+    Copyright (C) 2006 The Lobo Project. Copyright (C) 2014 - 2017 Lobo Evolution
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public
+    License as published by the Free Software Foundation; either
+    verion 3 of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General License for more details.
+
+    You should have received a copy of the GNU General Public
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
+
+    Contact info: lobochief@users.sourceforge.net; ivan.difrancesco@yahoo.it
+ */
+
+package org.lobobrowser.w3c.smil;
+
+/**
+ *  This interface represents the SMIL <code>animate</code> element. 
+ */
+public interface SMILAnimateElement extends SMILAnimation {
+}
+
