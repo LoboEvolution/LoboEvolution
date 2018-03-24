06ed4e961596c9634bf20baa9b5b3eb97ce2a057
oswetto <ivan.difrancesco@yahoo.it>
2018-03-25 00:02:52 +0100

    Added icon and bugfixing

--- Primary_Extension/src/main/java/org/loboevolution/primary/ext/InfoPageWindow.java
+++ Primary_Extension/src/main/java/org/loboevolution/primary/ext/InfoPageWindow.java
@@ -22,6 +22,7 @@ package org.loboevolution.primary.ext;
 
 import java.awt.BorderLayout;
 import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.Image;
 import java.awt.Toolkit;
 import java.io.IOException;
@@ -66,8 +67,6 @@ public class InfoPageWindow extends JFrame {
 
 	/** The Constant serialVersionUID. */
 	private static final long serialVersionUID = 1L;
-	
-	private RSyntaxTextArea textArea;
 
 	/**
 	 * Instantiates a new text viewer window.
@@ -76,17 +75,35 @@ public class InfoPageWindow extends JFrame {
 		super("Lobo Info");
 		setResizable(true);
 		setLocationRelativeTo(null);
-		JTabbedPane tabbedPane = new JTabbedPane();
-		tabbedPane.addTab("Info Page", infoContent(infoList));
-		tabbedPane.addTab("Media", mediaContent(mediaList));
-		tabbedPane.addTab("Css", content(cssList, SyntaxConstants.SYNTAX_STYLE_CSS));
-		tabbedPane.addTab("JS", content(jsList, SyntaxConstants.SYNTAX_STYLE_JAVASCRIPT));
-		add(tabbedPane, BorderLayout.CENTER);
-	}
 
-	@Override
-	public Image getIconImage() {
-		return new ImageIcon(getClass().getResource("/org/loboevolution/images/info.png")).getImage();
+		ImageIcon info = new ImageIcon(getClass().getResource("/org/loboevolution/images/info.png"));
+		ImageIcon media = new ImageIcon(getClass().getResource("/org/loboevolution/images/media.png"));
+		ImageIcon css = new ImageIcon(getClass().getResource("/org/loboevolution/images/css.png"));
+		ImageIcon js = new ImageIcon(getClass().getResource("/org/loboevolution/images/js.png"));
+
+		Image img = info.getImage();
+		Image newimg = img.getScaledInstance(30, 30, Image.SCALE_SMOOTH);
+		info = new ImageIcon(newimg);
+
+		img = media.getImage();
+		newimg = img.getScaledInstance(30, 30, Image.SCALE_SMOOTH);
+		media = new ImageIcon(newimg);
+
+		img = css.getImage();
+		newimg = img.getScaledInstance(30, 30, Image.SCALE_SMOOTH);
+		css = new ImageIcon(newimg);
+
+		img = js.getImage();
+		newimg = img.getScaledInstance(30, 30, Image.SCALE_SMOOTH);
+		js = new ImageIcon(newimg);
+
+		JTabbedPane tabbedPane = new JTabbedPane();
+		tabbedPane.setPreferredSize(new Dimension(30, 30));
+		tabbedPane.addTab("", info, infoContent(infoList));
+		tabbedPane.addTab("", media, mediaContent(mediaList));
+		tabbedPane.addTab("", css, content(cssList, SyntaxConstants.SYNTAX_STYLE_CSS));
+		tabbedPane.addTab("", js, jsContent(jsList, SyntaxConstants.SYNTAX_STYLE_JAVASCRIPT));
+		add(tabbedPane, BorderLayout.CENTER);
 	}
 
 	@Override
@@ -96,31 +113,31 @@ public class InfoPageWindow extends JFrame {
 
 	private JScrollPane infoContent(List<MetaInfo> infoList) {
 		try {
-			Object columnNames[] = {"", ""};
+			Object columnNames[] = { "", "" };
 			List<String[]> values = new ArrayList<String[]>();
-			for (MetaInfo info : infoList) {				
+			for (MetaInfo info : infoList) {
 				if (!Strings.isBlank(info.getName())) {
-					values.add(new String[] { info.getName(), info.getContent()});
+					values.add(new String[] { info.getName(), info.getContent() });
 				} else if (!Strings.isBlank(info.getItemprop())) {
-					values.add(new String[] { info.getItemprop(), info.getContent()});
+					values.add(new String[] { info.getItemprop(), info.getContent() });
 				} else if (!Strings.isBlank(info.getProperty())) {
-					values.add(new String[] { info.getProperty(), info.getContent()});
+					values.add(new String[] { info.getProperty(), info.getContent() });
 				} else if (!Strings.isBlank(info.getHttpEqui())) {
-					values.add(new String[] { info.getHttpEqui(), info.getContent()});
+					values.add(new String[] { info.getHttpEqui(), info.getContent() });
 				}
-			}			
+			}
 			DefaultTableModel model = new DefaultTableModel(values.toArray(new Object[][] {}), columnNames);
 			JTable jtable = new JTable(model);
 			jtable.setPreferredScrollableViewportSize(jtable.getPreferredSize());
-	        jtable.setTableHeader(null);
+			jtable.setTableHeader(null);
 			jtable.setShowGrid(false);
-			return new JScrollPane(jtable);			
+			return new JScrollPane(jtable);
 		} catch (Exception e) {
-			logger.error(e);
+			logger.error("infopage", e);
 		}
 		return null;
 	}
-	
+
 	private JScrollPane content(List<MetaInfo> infoList, String syntax) {
 		try {
 			Object columnNames[] = { "" };
@@ -137,50 +154,50 @@ public class InfoPageWindow extends JFrame {
 			jtable.setShowGrid(false);
 			jtable.setColumnSelectionAllowed(true);
 			jtable.setCellSelectionEnabled(true);
-		    
-			textArea = new RSyntaxTextArea();
-		    textArea.setHighlightCurrentLine(true);
+
+			RSyntaxTextArea textArea = new RSyntaxTextArea();
+			textArea.setHighlightCurrentLine(true);
 			textArea.setAnimateBracketMatching(true);
 			textArea.setAntiAliasingEnabled(true);
 			textArea.setEditable(false);
 			textArea.setSyntaxEditingStyle(syntax);
-			
+
 			ListSelectionModel cellSelectionModel = jtable.getSelectionModel();
-		    cellSelectionModel.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		    cellSelectionModel.addListSelectionListener(new ListSelectionListener() {
-		      public void valueChanged(ListSelectionEvent e) {   
+			cellSelectionModel.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+			cellSelectionModel.addListSelectionListener(new ListSelectionListener() {
+				public void valueChanged(ListSelectionEvent e) {
 
-		        int[] selectedRow = jtable.getSelectedRows();
-		        int[] selectedColumns = jtable.getSelectedColumns();
+					int[] selectedRow = jtable.getSelectedRows();
+					int[] selectedColumns = jtable.getSelectedColumns();
 
-		        for (int i = 0; i < selectedRow.length; i++) {
-		          for (int j = 0; j < selectedColumns.length; j++) {
-		        	  String href = (String) jtable.getValueAt(selectedRow[i], selectedColumns[j]);
-		        	  textArea.setText(getSourceCode(href,syntax));
-		        	  textArea.repaint();
-		          }
-		        }      
-		      }
-		    });
-		    	    
+					for (int i = 0; i < selectedRow.length; i++) {
+						for (int j = 0; j < selectedColumns.length; j++) {
+							String href = (String) jtable.getValueAt(selectedRow[i], selectedColumns[j]);
+							textArea.setText(getSourceCode(href, syntax));
+							textArea.repaint();
+						}
+					}
+				}
+			});
+
 			JPanel tablePanel = new JPanel();
-	        tablePanel.setLayout(new BoxLayout(tablePanel, BoxLayout.Y_AXIS));
-	        tablePanel.add(jtable);
-	        tablePanel.add(textArea); 
+			tablePanel.setLayout(new BoxLayout(tablePanel, BoxLayout.Y_AXIS));
+			tablePanel.add(jtable);
+			tablePanel.add(textArea);
 			return new JScrollPane(tablePanel);
 		} catch (Exception e) {
-			logger.error(e);
+			logger.error("infopage", e);
 		}
 		return null;
 	}
-	
-	private Component mediaContent(List<MetaInfo> mediaList) {
+
+	private JScrollPane jsContent(List<MetaInfo> infoList, String syntax) {
 		try {
 			Object columnNames[] = { "" };
 			List<String[]> values = new ArrayList<String[]>();
-			for (MetaInfo info : mediaList) {
+			for (MetaInfo info : infoList) {
 				if (!Strings.isBlank(info.getName())) {
-					values.add(new String[] { info.getName()});
+					values.add(new String[] { info.getName() });
 				}
 			}
 			DefaultTableModel model = new DefaultTableModel(values.toArray(new Object[][] {}), columnNames);
@@ -190,38 +207,91 @@ public class InfoPageWindow extends JFrame {
 			jtable.setShowGrid(false);
 			jtable.setColumnSelectionAllowed(true);
 			jtable.setCellSelectionEnabled(true);
-			
-			JPanel jPanelImg = new JPanel();
-		    			
-			ListSelectionModel cellSelectionModel = jtable.getSelectionModel();
-		    cellSelectionModel.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		    cellSelectionModel.addListSelectionListener(new ListSelectionListener() {
-		      public void valueChanged(ListSelectionEvent e) {   
-		    	jPanelImg.removeAll();
-		        int[] selectedRow = jtable.getSelectedRows();
-		        int[] selectedColumns = jtable.getSelectedColumns();
 
-		        for (int i = 0; i < selectedRow.length; i++) {
-		          for (int j = 0; j < selectedColumns.length; j++) {
-		        	  String href = (String) jtable.getValueAt(selectedRow[i], selectedColumns[j]);
-		        	  jPanelImg.add(new JLabel(new ImageIcon(getImage(href))));
-		        	  jPanelImg.repaint();
-		          }
-		        }      
-		      }
-		    });
-		    	    
+			RSyntaxTextArea textArea = new RSyntaxTextArea();
+			textArea.setHighlightCurrentLine(true);
+			textArea.setAnimateBracketMatching(true);
+			textArea.setAntiAliasingEnabled(true);
+			textArea.setEditable(false);
+			textArea.setSyntaxEditingStyle(syntax);
+
+			ListSelectionModel cellSelectionModel = jtable.getSelectionModel();
+			cellSelectionModel.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+			cellSelectionModel.addListSelectionListener(new ListSelectionListener() {
+				public void valueChanged(ListSelectionEvent e) {
+
+					int[] selectedRow = jtable.getSelectedRows();
+					int[] selectedColumns = jtable.getSelectedColumns();
+
+					for (int i = 0; i < selectedRow.length; i++) {
+						for (int j = 0; j < selectedColumns.length; j++) {
+							String href = (String) jtable.getValueAt(selectedRow[i], selectedColumns[j]);
+							textArea.setText(getSourceCode(href, syntax));
+							textArea.repaint();
+						}
+					}
+				}
+			});
+
 			JPanel tablePanel = new JPanel();
-	        tablePanel.setLayout(new BoxLayout(tablePanel, BoxLayout.Y_AXIS));
-	        tablePanel.add(jtable);
-	        tablePanel.add(jPanelImg); 
+			tablePanel.setLayout(new BoxLayout(tablePanel, BoxLayout.Y_AXIS));
+			tablePanel.add(jtable);
+			tablePanel.add(textArea);
 			return new JScrollPane(tablePanel);
 		} catch (Exception e) {
-			logger.error(e);
+			logger.error("infopage", e);
 		}
 		return null;
 	}
-	
+
+	private Component mediaContent(List<MetaInfo> mediaList) {
+		try {
+			Object columnNames[] = { "" };
+			List<String[]> values = new ArrayList<String[]>();
+			for (MetaInfo info : mediaList) {
+				if (!Strings.isBlank(info.getName())) {
+					values.add(new String[] { info.getName() });
+				}
+			}
+			DefaultTableModel model = new DefaultTableModel(values.toArray(new Object[][] {}), columnNames);
+			JTable jtable = new JTable(model);
+			jtable.setPreferredScrollableViewportSize(jtable.getPreferredSize());
+			jtable.setTableHeader(null);
+			jtable.setShowGrid(false);
+			jtable.setColumnSelectionAllowed(true);
+			jtable.setCellSelectionEnabled(true);
+
+			JPanel jPanelImg = new JPanel();
+
+			ListSelectionModel cellSelectionModel = jtable.getSelectionModel();
+			cellSelectionModel.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+			cellSelectionModel.addListSelectionListener(new ListSelectionListener() {
+				public void valueChanged(ListSelectionEvent e) {
+					jPanelImg.removeAll();
+					int[] selectedRow = jtable.getSelectedRows();
+					int[] selectedColumns = jtable.getSelectedColumns();
+
+					for (int i = 0; i < selectedRow.length; i++) {
+						for (int j = 0; j < selectedColumns.length; j++) {
+							String href = (String) jtable.getValueAt(selectedRow[i], selectedColumns[j]);
+							jPanelImg.add(new JLabel(new ImageIcon(getImage(href))));
+							jPanelImg.repaint();
+						}
+					}
+				}
+			});
+
+			JPanel tablePanel = new JPanel();
+			tablePanel.setLayout(new BoxLayout(tablePanel, BoxLayout.Y_AXIS));
+			tablePanel.add(jtable);
+			tablePanel.add(jPanelImg);
+			return new JScrollPane(tablePanel);
+		} catch (Exception e) {
+			logger.error("infopage", e);
+		}
+		return null;
+	}
+
 	private Image getImage(String href) {
 		try {
 			URL u = new URL(href);
@@ -249,12 +319,11 @@ public class InfoPageWindow extends JFrame {
 				return ImageIO.read(con.getInputStream());
 			}
 		} catch (Exception e) {
-			logger.error(e);
+			logger.error("infopage", e);
 		}
 		return null;
 	}
-	
-	
+
 	private String getSourceCode(String href, String syntax) {
 		SSLCertificate.setCertificate();
 		String source = "";
@@ -269,14 +338,14 @@ public class InfoPageWindow extends JFrame {
 			try {
 				content = IORoutines.load(in, 8192);
 				source = new String(content, "UTF-8");
-				if(SyntaxConstants.SYNTAX_STYLE_CSS.equals(syntax)) {
+				if (SyntaxConstants.SYNTAX_STYLE_CSS.equals(syntax)) {
 					source = source.replace("}", "}\n");
 				}
 			} finally {
 				in.close();
 			}
 		} catch (Exception e) {
-			logger.error(e);
+			logger.error("infopage", e);
 		}
 		return source;
 	}
