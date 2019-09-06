957d57cb751eb64b6c6979ffe55a5dbdf1150f4c
oswetto <ivan.difrancesco@yahoo.it>
2019-09-07 00:49:13 +0200

    Close browser clean tab cache

--- LoboCommon/src/main/java/org/lobo/store/TabStore.java
+++ LoboCommon/src/main/java/org/lobo/store/TabStore.java
@@ -14,15 +14,14 @@ public class TabStore {
 	private static String INSERT_TAB = "INSERT INTO TAB (index_tab, url) VALUES(?,?)";
 
 	private static String DELETE_TAB = "DELETE FROM TAB WHERE index_tab = ?";
+	
+	private static String DELETE_TAB_ALL = "DELETE FROM TAB";
 
 	private static String TAB = "SELECT url FROM TAB WHERE index_tab = ?";
 
 	private static String TABS = "SELECT index_tab, url FROM TAB";
 	
-
 	public static void insertTab(Integer index, String url) {
-		
-		
 		try (Connection conn = DriverManager.getConnection(SQLiteCommon.getDatabaseDirectory());
 				PreparedStatement pstmt = conn.prepareStatement(INSERT_TAB)) {
 			pstmt.setInt(1, index);
@@ -37,6 +36,15 @@ public class TabStore {
 		try (Connection conn = DriverManager.getConnection(SQLiteCommon.getDatabaseDirectory());
 				PreparedStatement pstmt = conn.prepareStatement(DELETE_TAB)) {
 			pstmt.setInt(1, index);
+			pstmt.executeUpdate();
+		} catch (final Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void deleteAll() {
+		try (Connection conn = DriverManager.getConnection(SQLiteCommon.getDatabaseDirectory());
+				PreparedStatement pstmt = conn.prepareStatement(DELETE_TAB_ALL)) {
 			pstmt.executeUpdate();
 		} catch (final Exception e) {
 			e.printStackTrace();
--- LoboEvo/src/main/java/org/lobo/component/BrowserFrame.java
+++ LoboEvo/src/main/java/org/lobo/component/BrowserFrame.java
@@ -14,6 +14,7 @@ import javax.swing.JPanel;
 
 import org.lobo.menu.MenuBar;
 import org.lobo.store.GeneralStore;
+import org.lobo.store.TabStore;
 import org.lobo.welcome.WelcomePanel;
 
 public class BrowserFrame extends JFrame implements IBrowserFrame {
@@ -65,7 +66,7 @@ public class BrowserFrame extends JFrame implements IBrowserFrame {
 		addWindowListener(new WindowAdapter() {
 			@Override
 			public void windowClosing(WindowEvent e) {
-				// TODO
+				TabStore.deleteAll();
 			}
 		});
 	}
