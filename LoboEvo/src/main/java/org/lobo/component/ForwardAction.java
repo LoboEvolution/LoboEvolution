8bf23893a66c91d0a54d609ce8fdc3d8221aef41
oswetto <ivan.difrancesco@yahoo.it>
2019-09-11 00:54:53 +0200

    Navigation history with back and forward button

--- LoboEvo/src/main/java/org/lobo/component/ForwardAction.java
+++ LoboEvo/src/main/java/org/lobo/component/ForwardAction.java
@@ -1,19 +1,20 @@
 package org.lobo.component;
 
 import java.awt.event.ActionEvent;
+import java.util.List;
 
 import javax.swing.AbstractAction;
 import javax.swing.JTextField;
 
 import org.lobo.common.Strings;
-import org.lobo.http.NavigationManager;
+import org.lobo.http.NavigationHistory;
 import org.lobo.store.TabStore;
 import org.lobo.tab.DnDTabbedPane;
 import org.lobo.tab.TabbedPanePopupMenu;
 import org.lobobrowser.html.dom.domimpl.HTMLDocumentImpl;
 import org.lobobrowser.html.gui.HtmlPanel;
 
-public class GoAction extends AbstractAction {
+public class ForwardAction extends AbstractAction {
 
 	private static final long serialVersionUID = 1L;
 
@@ -21,18 +22,29 @@ public class GoAction extends AbstractAction {
 
 	private final IBrowserPanel panel;
 
-	public GoAction(IBrowserPanel panel, JTextField addressBar) {
+	public ForwardAction(IBrowserPanel panel, JTextField addressBar) {
 		this.panel = panel;
 		this.addressBar = addressBar;
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent evt) {
+		NavigationHistory nh = new NavigationHistory();
+		final String url = this.addressBar.getText();
+		String text = url;
+		final int indexPanel = this.panel.getTabbedPane().getSelectedIndex();
+		List<String> tabsById = nh.getHostOrdered(indexPanel);
+		
+		for (int i = 0; i < tabsById.size(); i++) {
+			String tab = tabsById.get(i);
+			if(tab.equals(url) && i < tabsById.size()-1) {
+				text = tabsById.get(i + 1);
+			}
+		}
+						
 		final DnDTabbedPane tabbedPane = this.panel.getTabbedPane();
 		tabbedPane.setComponentPopupMenu(new TabbedPanePopupMenu(this.panel));
-		final int indexPanel = tabbedPane.getSelectedIndex();
-		final String text = this.addressBar.getText();
-		final HtmlPanel htmlPanel = NavigationManager.getHtmlPanel(text, indexPanel);
+		final HtmlPanel htmlPanel = HtmlPanel.createHtmlPanel(text);
 		htmlPanel.setBrowserPanel(panel);
 		
 		final HTMLDocumentImpl nodeImpl = (HTMLDocumentImpl) htmlPanel.getRootNode();
--- LoboEvo/src/main/java/org/lobo/http/NavigationHistory.java
+++ LoboEvo/src/main/java/org/lobo/http/NavigationHistory.java
@@ -15,12 +15,14 @@ public class NavigationHistory {
 	private final String DELETE_HOST = "DELETE FROM HOST";
 
 	private final String HOST = "SELECT DISTINCT baseUrl FROM HOST LIMIT ?";
+	
+	private final String HOST_ORDERED = "SELECT baseUrl FROM HOST where tab = ? ORDER BY dt ASC";
 
-	private final String INSERT_HOST = "INSERT INTO HOST (baseUrl, tab) VALUES(?,?)";
+	private final String INSERT_HOST = "INSERT INTO HOST (baseUrl, tab, dt) VALUES(?,?, strftime('%Y-%m-%d %H:%M:%S', 'now'))";
 
 	public void addAsRecent(String uri, int index) {
 		try (Connection conn = DriverManager.getConnection(SQLiteCommon.getDatabaseDirectory());
-				PreparedStatement pstmt = conn.prepareStatement(this.INSERT_HOST)) {
+				PreparedStatement pstmt = conn.prepareStatement(INSERT_HOST)) {
 			pstmt.setString(1, new URL(uri).toExternalForm());
 			pstmt.setInt(2, index);
 			pstmt.executeUpdate();
@@ -31,17 +33,33 @@ public class NavigationHistory {
 
 	public void deleteHost() {
 		try (Connection conn = DriverManager.getConnection(SQLiteCommon.getDatabaseDirectory());
-				PreparedStatement pstmt = conn.prepareStatement(this.DELETE_HOST)) {
+				PreparedStatement pstmt = conn.prepareStatement(DELETE_HOST)) {
 			pstmt.executeUpdate();
 		} catch (final Exception e) {
 			e.printStackTrace();
 		}
 	}
+	
+	public List<String> getHostOrdered(int index) {
+		final List<String> recentHostEntries = new ArrayList<String>();
+		try (Connection conn = DriverManager.getConnection(SQLiteCommon.getDatabaseDirectory());
+				PreparedStatement pstmt = conn.prepareStatement(HOST_ORDERED)) {
+			pstmt.setInt(1, index);
+			try (ResultSet rs = pstmt.executeQuery()) {
+				while (rs != null && rs.next()) {
+					recentHostEntries.add(rs.getString(1));
+				}
+			}
+		} catch (final Exception e) {
+			e.printStackTrace();
+		}
+		return recentHostEntries;
+	}
 
 	public List<String[]> getRecentHostEntries(int maxNumItems) {
 		final List<String[]> recentHostEntries = new ArrayList<String[]>();
 		try (Connection conn = DriverManager.getConnection(SQLiteCommon.getDatabaseDirectory());
-				PreparedStatement pstmt = conn.prepareStatement(this.HOST)) {
+				PreparedStatement pstmt = conn.prepareStatement(HOST)) {
 			pstmt.setInt(1, maxNumItems);
 			try (ResultSet rs = pstmt.executeQuery()) {
 				while (rs != null && rs.next()) {
