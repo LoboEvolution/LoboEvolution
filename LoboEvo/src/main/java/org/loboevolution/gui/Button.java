f5ac71a7329b67d75c80af5d4d76d7574a82b9f4
oswetto <ivan.difrancesco@yahoo.it>
2020-07-14 00:06:38 +0200

    Modern UI JButton

---
+++ LoboEvo/src/main/java/org/loboevolution/gui/Button.java
@@ -0,0 +1,23 @@
+package org.loboevolution.gui;
+
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.JButton;
+import javax.swing.border.LineBorder;
+
+public class Button extends JButton {
+
+	private static final long serialVersionUID = 1L;
+
+	/** The color text. */
+	private final Color COLOR_TEXT = new Color(108, 216, 158);
+
+	public Button() {
+		setForeground(COLOR_TEXT);
+		setFont(new Font("Tahoma", Font.BOLD, 14));
+		setFocusPainted(false);
+		setContentAreaFilled(false);
+		setBorder(new LineBorder(COLOR_TEXT));
+	}
+}
